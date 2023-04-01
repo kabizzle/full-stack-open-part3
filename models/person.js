4da0117376827286bc66d88fdc0a9e5fb2385717
Kabir Bissessar <kabirrbissessar@gmail.com>
2023-04-01 20:33:53 +0300

    added eslint, fixed linting errors

--- models/person.js
+++ models/person.js
@@ -1,46 +1,46 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require("mongoose")
+require("dotenv").config()
 
-mongoose.set('strictQuery',false)
+mongoose.set("strictQuery",false)
 
 const url = process.env.MONGODB_URI
 
 // console.log("Connecting to ", url)
 
 mongoose.connect(url)
-.then( result => {
-    console.log("Connected to MongoDB")
-})
-.catch( (error) => {
-    console.log("Error connecting to MongoDB: ", error.message)
-})  
+	.then( () => {
+		console.log("Connected to MongoDB")
+	})
+	.catch( (error) => {
+		console.log("Error connecting to MongoDB: ", error.message)
+	})  
 
-const validatePhoneNumber = (num) => {
-    return /\d{2,}-\d{7,}/.test(num)
-}
+// const validatePhoneNumber = (num) => {
+//     return /\d{2,}-\d{7,}/.test(num)
+// }
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String, 
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        validate: {
-            validator: function(num) {
-                return /\d{2,}-\d{7,}/.test(num)
-            }, message: "not valid phone number"
-        },
-        required: [true, "Phone number is not correct"]
-    },
+	name: {
+		type: String, 
+		minLength: 3,
+		required: true
+	},
+	number: {
+		type: String,
+		validate: {
+			validator: function(num) {
+				return /\d{2,}-\d{7,}/.test(num)
+			}, message: "not valid phone number"
+		},
+		required: [true, "Phone number is not correct"]
+	},
 })
 
-personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+personSchema.set("toJSON", {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
 })
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model("Person", personSchema)
--- mongo.js
+++ mongo.js
@@ -1,49 +1,48 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+	console.log("give password as argument")
+	process.exit(1)
 }
 
 const password = process.argv[2]
 
-const url =
-  `mongodb+srv://kabir:${password}@fullstack.bpbtuix.mongodb.net/Phonebook?retryWrites=true&w=majority`
+const url = `mongodb+srv://kabir:${password}@fullstack.bpbtuix.mongodb.net/Phonebook?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery',false)
+mongoose.set("strictQuery", false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+	name: String,
+	number: String,
 })
 
-const Person = mongoose.model('Person', personSchema)
+const Person = mongoose.model("Person", personSchema)
 
 if (process.argv.length == 3) {
-    console.log("Phonebook: ")
-    Person
-    .find({})
-    .then( result => {
-        result.forEach( person => {
-            console.log(person.name, person.number)
-        })
-        mongoose.connection.close()
-    })
+	console.log("Phonebook: ")
+	Person
+		.find({})
+		.then( result => {
+			result.forEach( person => {
+				console.log(person.name, person.number)
+			})
+			mongoose.connection.close()
+		})
 }
 
 else if (process.argv.length == 5) {
-    const personName = process.argv[3]
-    const personNumber = process.argv[4]
+	const personName = process.argv[3]
+	const personNumber = process.argv[4]
+
+	const person = new Person({
+		name: personName,
+		number: personNumber,
+	})
     
-    const person = new Person({
-      name: personName,
-      number: personNumber,
-    })
-    
-    person.save().then(result => {
-      console.log(`Added ${personName} ${personNumber} to phonebook`)
-      mongoose.connection.close()
-    })
+	person.save().then( () => {
+		console.log(`Added ${personName} ${personNumber} to phonebook`)
+		mongoose.connection.close()
+	})
 }
 
